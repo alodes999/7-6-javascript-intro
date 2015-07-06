e194276a90666fb721618cb2c2dcaeceaf4a3a1e
Andrew Lodes <alodes999@gmail.com>
2015-07-06 11:03:31 -0500

    Atomic 2 file content finished
    Documentation needs added yet

---
+++ atomic2/atomic2.js
@@ -0,0 +1,13 @@
+alert("Hello! My name is Andrew.  How are you?");
+
+console.log("My favorite breakfast is waffles.");
+
+console.log("Currently (and more than likely permanently) 0 cats reside with me.");
+
+console.log("I have 1 brother, and a couple half-siblings.");
+
+console.log("I currently take between 0 and 1 nap a week");
+
+console.log("I don't much like taking naps, but I do (and should) need to get more sleep at night!");
+
+alert("Goodbye user! Thanks for listening!")
