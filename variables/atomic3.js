1d10e0a47eead5009e37e8258ac360f1be89ae59
Andrew Lodes <alodes999@gmail.com>
2015-07-06 11:55:47 -0500

    Add documentation to atomic 3

--- variables/atomic3.js
+++ variables/atomic3.js
@@ -1,9 +1,10 @@
+//These first 3 lines define the variables needed for the next portion of our JS code
 var name = 'Andrew';
 
-var age;
+var age = 32; 
 
 var hairColor = 'blonde';
-
+//These are the alerts that pop up displaying the information defined in our variables above.
 alert("My name is " + name);
 
 alert("My age is " + age);
