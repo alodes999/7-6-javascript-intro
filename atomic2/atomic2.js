c750ceea2a0e11b5bdf916b06d53495982b71b2a
Andrew Lodes <alodes999@gmail.com>
2015-07-06 11:07:18 -0500

    Add documentation for js file

--- atomic2/atomic2.js
+++ atomic2/atomic2.js
@@ -1,5 +1,6 @@
+//This is the opening alert once the page loads.  The page will spin waiting for the Ok to be clicked.
 alert("Hello! My name is Andrew.  How are you?");
-
+//Here are out console.log outputs.  These aren't seen on the main page, but they are seen in our dev console. This is shown in the console after OK is pressed on the first alert, but before you click Ok for the second alert
 console.log("My favorite breakfast is waffles.");
 
 console.log("Currently (and more than likely permanently) 0 cats reside with me.");
@@ -9,5 +10,5 @@ console.log("I have 1 brother, and a couple half-siblings.");
 console.log("I currently take between 0 and 1 nap a week");
 
 console.log("I don't much like taking naps, but I do (and should) need to get more sleep at night!");
-
+//This is the closing alert.  Once the user clicks OK for the first alert, this displays and needs to be clicked through before the html base shows for the page.
 alert("Goodbye user! Thanks for listening!")
