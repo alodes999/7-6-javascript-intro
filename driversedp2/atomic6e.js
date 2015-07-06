3c9059765edd387874ff7cbb706d500cb772c97f
Andrew Lodes <alodes999@gmail.com>
2015-07-06 17:43:46 -0500

    Add documentation to js

--- driversedp2/atomic6e.js
+++ driversedp2/atomic6e.js
@@ -1,7 +1,8 @@
+//Sets the user's entered age from the users input.
 var userage = prompt("Please enter your age, to see if you can drive!");
-
+//Sets the users entered age as a variable for our loop.
 var checkage = parseInt(userage, 10);
-
+//Our loop logic, the variable is checked against each value until one is found to be true, and then that result is output.
 if (checkage >= 25) {
   alert("You can rent a car");
 } else if (checkage >= 21 && checkage < 25) {
